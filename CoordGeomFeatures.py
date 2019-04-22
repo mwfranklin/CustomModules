f5e979744d999dbe29a3298ddae7f242c3e421a2
Meghan W Franklin <mwfranklin@ku.edu>
2019-04-22 13:06:56 -0500

    Bug fixing still for Mg

--- CoordGeomFeatures.py
+++ CoordGeomFeatures.py
@@ -104,11 +104,14 @@ def bond_valences(ligands, coords, metal, charge, bond_params):
         try:
             this_param = bond_params[(bond_params.Metal.str.upper() == metal) & (bond_params.Charge == charge) & (bond_params.CoordAtom == this_ligand)].values[0]
         except IndexError: #a charge of 9 is code for "all" or "unknown" so it is more likely to find the correct metal:coord atom pair
-            try:
-                this_param = bond_params[(bond_params.Metal.str.upper() == metal) & (bond_params.Charge == 9) & (bond_params.CoordAtom == this_ligand)].values[0]
-            except IndexError:
-                #when *ALL* else fails, calculate for bonded with As
-                this_param = bond_params[(bond_params.Metal.str.upper() == metal) & (bond_params.Charge == 9) & (bond_params.CoordAtom == "As")].values[0]
+            if metal = "MG": #Mg can only have a charge of +2
+                this_param = bond_params[(bond_params.Metal.str.upper() == metal) & (bond_params.Charge == 2) & (bond_params.CoordAtom == "As")].values[0]
+            else:
+                try:
+                    this_param = bond_params[(bond_params.Metal.str.upper() == metal) & (bond_params.Charge == 9) & (bond_params.CoordAtom == this_ligand)].values[0]
+                except IndexError:
+                    #when *ALL* else fails, calculate for bonded with As
+                    this_param = bond_params[(bond_params.Metal.str.upper() == metal) & (bond_params.Charge == 9) & (bond_params.CoordAtom == "As")].values[0]
                 
         r0 = this_param[4]
         b = this_param[5]

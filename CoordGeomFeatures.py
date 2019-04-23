d1d21945092407f9c262005f865ee9ccf9e6bad3
Meghan W Franklin <mwfranklin@ku.edu>
2019-04-23 14:49:25 -0500

    Bug fixes, again

--- CoordGeomFeatures.py
+++ CoordGeomFeatures.py
@@ -102,17 +102,17 @@ def bond_valences(ligands, coords, metal, charge, bond_params):
             this_ligand = this_ligand      
         this_dist = distances[x]
         print(this_ligand, this_dist)
-        print(bond_params[(bond_params.Metal.str.upper() == metal)])# & (bond_params.Charge == charge) & (bond_params.CoordAtom == this_ligand)] )
+        print(bond_params[(bond_params.Metal.str.upper() == metal) & (bond_params.Charge == charge) & (bond_params.CoordAtom.str.upper() == this_ligand)] )
         try:
-            this_param = bond_params[(bond_params.Metal.str.upper() == metal) & (bond_params.Charge == charge) & (bond_params.CoordAtom == this_ligand)].values[0]
+            this_param = bond_params[(bond_params.Metal.str.upper() == metal) & (bond_params.Charge == charge) & (bond_params.CoordAtom.str.upper() == this_ligand)].values[0]
         except IndexError: #a charge of 9 is code for "all" or "unknown" so it is more likely to find the correct metal:coord atom pair
             if metal == "MG": #Mg can only have a charge of +2
                 this_param = bond_params[(bond_params.Metal.str.upper() == metal) & (bond_params.Charge == 2) & (bond_params.CoordAtom == "As")].values[0]
             elif metal == "CU":
-                this_param = bond_params[(bond_params.Metal.str.upper() == metal) & (bond_params.Charge == 2) & (bond_params.CoordAtom == this_ligand)].values[0]
+                this_param = bond_params[(bond_params.Metal.str.upper() == metal) & (bond_params.Charge == 2) & (bond_params.CoordAtom.str.upper() == this_ligand)].values[0]
             else:
                 try:
-                    this_param = bond_params[(bond_params.Metal.str.upper() == metal) & (bond_params.Charge == 9) & (bond_params.CoordAtom == this_ligand)].values[0]
+                    this_param = bond_params[(bond_params.Metal.str.upper() == metal) & (bond_params.Charge == 9) & (bond_params.CoordAtom.str.upper() == this_ligand)].values[0]
                 except IndexError:
                     #when *ALL* else fails, calculate for bonded with As
                     this_param = bond_params[(bond_params.Metal.str.upper() == metal) & (bond_params.Charge == 9) & (bond_params.CoordAtom == "As")].values[0]
